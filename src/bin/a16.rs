3d827f5578daf19461e7bce42dae911d579b2225
dark-noob830 <mahdiamr83@gmail.com>
2025-11-05 10:27:08 +0330

    Add: a16

---
+++ src/bin/a16.rs
@@ -0,0 +1,26 @@
+// Topic: Option
+//
+// Requirements:
+// * Print out the details of a student's locker assignment
+// * Lockers use numbers and are optional for students
+//
+// Notes:
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+struct Student {
+    name: String,
+    locker: Option<i32>,
+}
+fn main() {
+    let student = Student {
+        name: "John".to_string(),
+        locker: Some(101),
+    };
+    match student.locker {
+        Some(locker) => println!("Student: {} has locker: {}", student.name, locker),
+        None => println!("Student: {} has no locker", student.name),
+    }
+}
