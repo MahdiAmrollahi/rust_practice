a0814f37625e0e27202af7b05234a26338160b4b
dark-noob830 <mahdiamr83@gmail.com>
2025-11-23 21:17:25 +0330

    add: 24

---
+++ src/bin/a24.rs
@@ -0,0 +1,20 @@
+// Topic: Iterator
+//
+// Requirements:
+// * Triple the value of each item in a vector.
+// * Filter the data to only include values > 10.
+// * Print out each element using a for loop.
+//
+// Notes:
+// * Use an iterator chain to accomplish the task.
+
+fn main() {
+    let data: Vec<_> = vec![1, 2, 3, 4, 5]
+        .iter()
+        .map(|x| x * 3)
+        .filter(|x| x > &10)
+        .collect();
+    for x in data {
+        println!("{}", x);
+    }
+}
